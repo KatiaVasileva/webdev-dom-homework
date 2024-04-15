87888761eb7b6e46d964f2ee4ab2e7bee6b700f7
katiavasileva <eka.vasileva@gmail.com>
2024-04-15 14:49:59 +0500

    Additional task (loading comment) is done

--- hw_12.js
+++ hw_12.js
@@ -8,17 +8,18 @@
     + при запуске приложение загружает список комментариев из API и выводит его на экран;
     + при добавлении комментария приложение сохраняет комментарий в API, а затем выводит добавленный комментарий в список.
 + Функционал лайков работает локально, то есть можно поставить и снять лайк, но после перезагрузки страницы счетчик лайков сбросится.
+
+Дополнительное задание
+Добавление коммента в API занимает некоторое время, и до тех пор, пока запрос не завершится, комментарий в списке еще не появится.
+Задача: показать пользователю, что с приложением всё в порядке, просто запрос еще не выполнился. 
+Для этого на время загрузки скроем форму добавления, вместо нее покажем текст «Комментарий добавляется»
 */
 
 const commentListElement = document.querySelector('#comment-list');
-const addFormButtonElement = document.querySelector('#add-form-button');
-const nameInputElement = document.querySelector('#name-input');
-const commentInputElement = document.querySelector('#comment-input');
-const inputFormElement = document.querySelector('#input-form');
 const removeCommentButtonElement = document.querySelector('#remove-comment-button');
-const likeButtonElements = document.querySelectorAll('.like-button');
+const addFormElement = document.querySelector('.add-form');
 
-// Массив для извлечения комментариев с сервера
+// Массив для сохранения комментариев, извлеченных из API
 let comments = [];
 
 // Функция с GET-запросом для получения списка комментариев
@@ -42,6 +43,7 @@ function getAllComments() {
             renderComments();
         })
     });
+
 };
 
 // Вызов функции с GET-запросом для получения списка комментариев из API
@@ -51,6 +53,7 @@ getAllComments();
 // счетчик увеличивается на единицу, при нажатии на закрашенное сердечко оно становится пустым и счетчик уменшается на единицу.
 const initLikeButtonListener = () => {
     const likeButtonElements = document.querySelectorAll('.like-button');
+
     for (const likeButtonElement of likeButtonElements) {
         likeButtonElement.addEventListener('click', (e) => {
             e.stopPropagation();
@@ -72,6 +75,7 @@ const initLikeButtonListener = () => {
 // на поле ввода, в которое подставлен текущий текст комментария, а кнопка "Редактировать" заменяется на кнопку "Сохранить".
 const initEditButtonListener = () => {
     const editButtonElements = document.querySelectorAll('.edit-comment-button');
+
     for (const editButtonElement of editButtonElements) {
         editButtonElement.addEventListener('click', (e) => {
             e.stopPropagation();
@@ -88,6 +92,7 @@ const initEditButtonListener = () => {
 // на отредактированный комментарий, который сохраняется в массиве JS, а кнопка "Сохранить" заменяется на кнопку "Редактировать".
 const initSaveButtonListener = () => {
     const saveButtonElements = document.querySelectorAll('.save-comment-button');
+
     for (const saveButtonElement of saveButtonElements) {
         saveButtonElement.addEventListener('click', (e) => {
             e.stopPropagation();
@@ -105,6 +110,7 @@ const initSaveButtonListener = () => {
 // не должен срабатывать при нажатии на лайк и на кнопки "Редактировать/Сохранить"
 const initCommentReplyListener = () => {
     const commentElements = document.querySelectorAll('.comment');
+
     for (const commentElement of commentElements) {
         commentElement.addEventListener('click', (e) => {
             if (e.target.classList.contains('edit-comment-form-text')) {
@@ -114,6 +120,31 @@ const initCommentReplyListener = () => {
             commentInputElement.value = `QUOTE_BEGIN ${comments[index].name}: \n ${comments[index].text}QUOTE_END \n \n`;
         });
     }
+}
+
+// Функция для получения даты и времени в формате: дд.мм.гг чч:мм
+function getTime(date) {
+    let timeOptions = { hour: '2-digit', minute: '2-digit' };
+    let formattedTime = date.toLocaleTimeString('ru-Ru', timeOptions);
+
+    let dateOptions = { day: '2-digit', month: '2-digit', year: '2-digit' };
+    let formattedDate = date.toLocaleDateString('ru-Ru', dateOptions);
+
+    return formattedDate + ' ' + formattedTime;
+}
+
+// Функция для дезактивирования кнопки "Написать"
+const disableButton = () => {
+    const addFormButtonElement = document.querySelector('.add-form-button');
+    addFormButtonElement.disabled = true;
+    addFormButtonElement.classList.add('add-form-button_disabled');
+}
+
+// Функция для активирования кнопки "Написать"
+const enableButton = () => {
+    const addFormButtonElement = document.querySelector('.add-form-button');
+    addFormButtonElement.disabled = false;
+    addFormButtonElement.classList.remove('add-form-button_disabled');
 }
 
 // Рендер-функция, которая отрисовывает список комментариев.
@@ -145,31 +176,42 @@ const renderComments = () => {
     // initCommentReplyListener();
 }
 
+// Рендер-функция, которая отрисовывает форму ввода комментрия.
+const renderInputBox = () => {
+    const inputBoxHtml = `
+    <input
+        id="name-input"
+        type="text"
+        class="add-form-name"
+        placeholder="Введите ваше имя"
+    />
+    <textarea
+        id="comment-input"
+        type="textarea"
+        class="add-form-text"
+        placeholder="Введите ваш комментарий"
+        rows="4"
+    ></textarea>
+    <div class="add-form-row">
+        <button id="add-form-button" class="add-form-button">Написать</button>
+    </div>`;
+    addFormElement.innerHTML = inputBoxHtml;
+
+    initNameInputListener();
+    initCommentInputListener();
+    initAddFormListener();
+
+    renderComments();
+
+    disableButton();
+
+}
+
 // Вызов рендер-функции для отрисовки списка комментариев
 renderComments();
 
-// Функция для получения даты и времени в формате: дд.мм.гг чч:мм
-const getTime = (date) => {
-    let timeOptions = { hour: '2-digit', minute: '2-digit' };
-    let formattedTime = date.toLocaleTimeString('ru-Ru', timeOptions);
-
-    let dateOptions = { day: '2-digit', month: '2-digit', year: '2-digit' };
-    let formattedDate = date.toLocaleDateString('ru-Ru', dateOptions);
-
-    return formattedDate + ' ' + formattedTime;
-}
-
-// Функция для дезактивирования кнопки "Написать"
-const disableButton = () => {
-    addFormButtonElement.disabled = true;
-    addFormButtonElement.classList.add('add-form-button_disabled');
-}
-
-// Функция для активирования кнопки "Написать"
-const enableButton = () => {
-    addFormButtonElement.disabled = false;
-    addFormButtonElement.classList.remove('add-form-button_disabled');
-}
+// Вызов рендер-функции для отрисовки формы ввода
+renderInputBox();
 
 // Изначально при пустых полях ввода кнопка "Написать" неактивна.
 disableButton();
@@ -177,54 +219,87 @@ disableButton();
 // Валидация полей ввода: кнопка "Написать" активна, только если оба поля ввода заполнены.
 // Если, заполнив оба поля, пользователь решил очистить значения в одном из полей, 
 // кнопка "Написать" становится неактивной.
-nameInputElement.addEventListener('input', (e) => {
-    if (e.target.value.trim() === '') {
-        disableButton();
-    }
-    if (e.target.value.trim() !== '' && commentInputElement.value.trim() !== '') {
-        enableButton();
-    }
-});
+function initNameInputListener() {
+    const nameInputElement = document.querySelector('#name-input');
+    const commentInputElement = document.querySelector('#comment-input');
 
-commentInputElement.addEventListener('input', (e) => {
-    if (e.target.value.trim() === '') {
-        disableButton();
-    }
-    if (e.target.value.trim() !== '' && nameInputElement.value.trim() !== '') {
-        enableButton();
-    }
-});
+    nameInputElement.addEventListener('input', (e) => {
+        if (e.target.value.trim() === '') {
+            disableButton();
+        }
+        if (e.target.value.trim() !== '' && commentInputElement.value.trim() !== '') {
+            enableButton();
+        }
+    });
+};
+
+function initCommentInputListener() {
+    const commentInputElement = document.querySelector('#comment-input');
+    const nameInputElement = document.querySelector('#name-input');
+
+    commentInputElement.addEventListener('input', (e) => {
+        if (e.target.value.trim() === '') {
+            disableButton();
+        }
+        if (e.target.value.trim() !== '' && nameInputElement.value.trim() !== '') {
+            enableButton();
+        }
+    });
+
+};
 
 // При нажатии на кнопку "Написать" отправляется POST-запрос для добавления нового комментария в API
 // и GET-запрос для получения актуального списка комментариев из API, после чего список комментариев отрисовывается на странице, 
 // поля ввода очищаются, кнопка "Написать" дезактивируется.
-addFormButtonElement.addEventListener('click', () => {
-    fetch(
-        "https://wedev-api.sky.pro/api/v1/katia-vasileva/comments",
-        {
-            method: "POST",
-            body: JSON.stringify({
-                text: commentInputElement.value.replaceAll("<", "&lt;").replaceAll(">", "&gt;"),
-                name: nameInputElement.value.replaceAll("<", "&lt;").replaceAll(">", "&gt;")
-                    .replaceAll('QUOTE_BEGIN', '<div class="quote">').replaceAll('QUOTE_END', '</div>')
-            })
-        }
-    ).then((response) => {
-        response.json().then((responseData) => {
-            comments = responseData.comments;
-        })
+function initAddFormListener() {
+    const addFormButtonElement = document.querySelector('.add-form-button');
+
+    addFormButtonElement.addEventListener('click', () => {
+
+        const nameInputElement = document.querySelector('#name-input');
+        const commentInputElement = document.querySelector('#comment-input');
+
+        addFormElement.innerHTML = `
+        <div class="comment-add-container">
+            <p>Комментарий добавляется...</p>
+            <img src="./spinner.svg" class="spinner">
+        </div>
+        `;
+
+        setTimeout(() => {
+            fetch(
+                "https://wedev-api.sky.pro/api/v1/katia-vasileva/comments",
+                {
+                    method: "POST",
+                    body: JSON.stringify({
+                        text: commentInputElement.value.replaceAll("<", "&lt;").replaceAll(">", "&gt;"),
+                        name: nameInputElement.value.replaceAll("<", "&lt;").replaceAll(">", "&gt;")
+                            .replaceAll('QUOTE_BEGIN', '<div class="quote">').replaceAll('QUOTE_END', '</div>')
+                    })
+                }
+            ).then((response) => {
+                response.json().then((responseData) => {
+                    comments = responseData.comments;
+                })
+            });
+
+            nameInputElement.value = '';
+            commentInputElement.value = '';
+
+            renderInputBox();
+
+            getAllComments();
+        }, 3000);
     });
+}
 
-    getAllComments();
-
-    nameInputElement.value = '';
-    commentInputElement.value = '';
-
-    disableButton();
-});
 
 // Добавление комментария на страницу по нажатию Enter
+const inputFormElement = document.querySelector('#input-form');
+
 inputFormElement.addEventListener('keyup', (e) => {
+    const addFormButtonElement = document.querySelector('.add-form-button');
+    
     if (e.key === 'Enter') {
         addFormButtonElement.click();
     }
