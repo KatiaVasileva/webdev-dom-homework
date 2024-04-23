69e44a25200ef6b6e58a9593ae4121e05f366ca2
katiavasileva <eka.vasileva@gmail.com>
2024-04-23 18:28:26 +0500

    Main and additional tasks are done (error handling)

--- hw.js
+++ hw.js
@@ -1,21 +1,15 @@
 /* Задание
-В этой домашке мы покажем пользователю приложения состояние, когда данные загружаются:
-- Я как пользователь при запуске приложения понимаю, что данные загружаются и мне нужно немного подождать.
-- Я как пользователь при добавлении комментария понимаю, что мне нужно подождать и не могу случайно отправить коммент повторно во время загрузки предыдущего.
++ Я, как пользователь, понимаю, что ввел слишком короткую строчку в имя или текст комментария при добавлении комментария.
++ Я, как пользователь, понимаю, что у меня пропал интернет и мне нужно повторить попытку добавить комментарий позже.
 
-+ Начните с того, что переделайте все вызовы then на цепочки промисов, вынесите код получения списка комментариев в отдельную функцию. 
-+ Используйте «замедление интернета» в консоли разработчика на вкладке сеть, чтобы отладить работу лоадеров.
+Что нужно сделать:
++ Обработать 400-ю ошибку в POST-запросе на добавление комментария и показать пользователю alert.
++ Обработать 500-е ошибки в запросе на получение комментов и на добавление коммента.
++ Обработать ошибки типа «у пользователя пропал интернет» во всех запросах.
++ Форма добавления не должна сбрасываться в случае ошибки.
 
 Дополнительное задание:
-Реализуем сценарий
-+ Я как пользователь понимаю, что лайк ставится/снимается и мне нужно подождать, пока он поставится.
-
-Условия проверки и выполнения задания:
-
-+ В коде приложения нет вложенных then, везде используются цепочки промисов.
-+ Целевые сценарии выполнены:
-    + при запуске приложения отображается сообщение о загрузке списка комментариев, при добавлении нового комментария это сообщение не отображается;
-    + при добавлении нового комментария, на время выполнения запроса, скрывается форма добавления, вместо нее отображается надпись «Комментарий добавляется».
++ Реализуйте автоматический повторный запрос в API в случае 500-го ответа.
 */
 
 const commentListElement = document.querySelector('#comment-list');
@@ -27,6 +21,8 @@ const commentLoaderElement = document.querySelector('.comment-loader');
 let comments = [];
 
 // Функция с GET-запросом для получения списка комментариев
+// В случае 500-го ответа выполняется автоматический повторный запрос в API.
+// В случае отсутствия интернета выводится alert.
 const getAllComments = () => {
 
     return fetch(
@@ -35,6 +31,9 @@ const getAllComments = () => {
             method: "GET"
         })
         .then((response) => {
+            if (response.status === 500) {
+                throw new Error("Ошибка сервера");
+            }
             return response.json();
         })
         .then((responseData) => {
@@ -46,10 +45,18 @@ const getAllComments = () => {
                     isLiked: comment.isLiked,
                     likes: comment.likes
                 }
-            });
+            })
             commentLoaderElement.classList.add('hide-comment-loader');
             renderComments();
         })
+        .catch((error) => {
+            console.warn(error);
+            if (error.message === "Ошибка сервера") {
+                getAllComments();
+            } else {
+                alert("Кажется, у вас сломался интернет, попробуйте позже");
+            };
+        });
 };
 
 // Вызов функции с GET-запросом для получения списка комментариев из API
@@ -187,13 +194,15 @@ const renderComments = () => {
 }
 
 // Рендер-функция, которая отрисовывает форму ввода комментрия.
-const renderInputBox = () => {
+const renderInputBox = (name, comment) => {
+
     const inputBoxHtml = `
     <input
         id="name-input"
         type="text"
         class="add-form-name"
         placeholder="Введите ваше имя (не менее трех символов)"
+        value="${name}"
     />
     <textarea
         id="comment-input"
@@ -201,7 +210,7 @@ const renderInputBox = () => {
         class="add-form-text"
         placeholder="Введите ваш комментарий (не менее трех символов)"
         rows="4"
-    ></textarea>
+    >${comment}</textarea>
     <div class="add-form-row">
         <button id="add-form-button" class="add-form-button">Написать</button>
     </div>`;
@@ -218,10 +227,14 @@ const renderInputBox = () => {
 }
 
 // Вызов рендер-функции для отрисовки формы ввода
-renderInputBox();
+renderInputBox("", "");
 
 // Изначально при пустых полях ввода кнопка "Написать" неактивна.
 disableButton();
+
+// Переменные для хранения текста, введенного пользователем
+let nameInput = "";
+let commentInput = "";
 
 // Валидация полей ввода: кнопка "Написать" активна, только если оба поля ввода заполнены.
 // Если, заполнив оба поля, пользователь решил очистить значения в одном из полей, 
@@ -229,14 +242,15 @@ disableButton();
 function initNameInputListener() {
     const nameInputElement = document.querySelector('#name-input');
     const commentInputElement = document.querySelector('#comment-input');
-
+    
     nameInputElement.addEventListener('input', (e) => {
-        if (e.target.value.trim().length < 3) {
+        if (e.target.value.trim().length === 0) {
             disableButton();
         }
-        if (commentInputElement.value.trim() !== '' && e.target.value.trim().length >= 3) {
+        if (commentInputElement.value.trim() !== '' && e.target.value.trim().length > 0) {
             enableButton();
         }
+        nameInput = e.target.value;
     });
 };
 
@@ -245,14 +259,73 @@ function initCommentInputListener() {
     const nameInputElement = document.querySelector('#name-input');
 
     commentInputElement.addEventListener('input', (e) => {
-        if (e.target.value.trim().length < 3) {
+        if (e.target.value.trim().length === 0) {
             disableButton();
         }
-        if (nameInputElement.value.trim() !== '' && e.target.value.trim().length >= 3) {
+        if (nameInputElement.value.trim() !== '' && e.target.value.trim().length > 0) {
             enableButton();
         }
+        commentInput = e.target.value;
     });
 
+};
+
+// Функция с POST-запросом для добавления нового комментария в API и GET-запросом для получения актуального списка комментариев из API
+// В случае 500-го ответа выполняется автоматический повторный запрос в API.
+// В случае 400-го ответа выводится alert, форма не очищается.
+// В случае отсутствия интернета выводится alert.
+const addComment = (name, comment) => {
+
+    return fetch(
+        "https://wedev-api.sky.pro/api/v1/katia-vasileva/comments",
+        {
+            method: "POST",
+            body: JSON.stringify({
+                text: comment.replaceAll("<", "&lt;").replaceAll(">", "&gt;"),
+                name: name.replaceAll("<", "&lt;").replaceAll(">", "&gt;")
+                    .replaceAll('QUOTE_BEGIN', '<div class="quote">').replaceAll('QUOTE_END', '</div>'),
+                forceError: true
+            })
+        }
+    )
+        .then((response) => {
+            if (response.status === 500) {
+                throw new Error("Ошибка сервера");
+            }
+
+            if (response.status === 400) {
+                console.log(response);
+                throw new Error("Плохой запрос");
+            }
+
+            addFormElement.innerHTML = `
+                <div class="comment-add-container">
+                    <p>Комментарий добавляется...</p>
+                    <img src="./spinner.svg" class="spinner">
+                </div>
+            `;
+            return response.json();
+        })
+        .then(() => {
+            return getAllComments();
+        })
+        .then(() => {
+            renderInputBox("", "");
+        })
+        .catch((error) => {
+            renderInputBox(nameInput, commentInput);
+
+            console.warn(error);
+
+            if (error.message === "Ошибка сервера") {
+                addComment(name, comment);
+            } else if (error.message === "Плохой запрос") {
+                alert("Имя и комментарий должны быть не короче трех символов");
+            } else {
+                alert("Кажется, у вас сломался интернет, попробуйте позже");
+                enableButton();
+            }
+        });
 };
 
 // При нажатии на кнопку "Написать" отправляется POST-запрос для добавления нового комментария в API
@@ -261,38 +334,13 @@ function initCommentInputListener() {
 function initAddFormListener() {
     const addFormButtonElement = document.querySelector('.add-form-button');
 
+    const commentInputElement = document.querySelector('#comment-input');
+    const nameInputElement = document.querySelector('#name-input');
+
     addFormButtonElement.addEventListener('click', () => {
 
-        const nameInputElement = document.querySelector('#name-input');
-        const commentInputElement = document.querySelector('#comment-input');
+        addComment(nameInputElement.value, commentInputElement.value);
 
-        addFormElement.innerHTML = `
-        <div class="comment-add-container">
-            <p>Комментарий добавляется...</p>
-            <img src="./spinner.svg" class="spinner">
-        </div>
-        `;
-
-        fetch(
-            "https://wedev-api.sky.pro/api/v1/katia-vasileva/comments",
-            {
-                method: "POST",
-                body: JSON.stringify({
-                    text: commentInputElement.value.replaceAll("<", "&lt;").replaceAll(">", "&gt;"),
-                    name: nameInputElement.value.replaceAll("<", "&lt;").replaceAll(">", "&gt;")
-                        .replaceAll('QUOTE_BEGIN', '<div class="quote">').replaceAll('QUOTE_END', '</div>')
-                })
-            }
-        )
-            .then((response) => {
-                return response.json();
-            })
-            .then(() => {
-                return getAllComments();
-            })
-            .then(() => {
-                renderInputBox();
-            })
     });
 };
 
